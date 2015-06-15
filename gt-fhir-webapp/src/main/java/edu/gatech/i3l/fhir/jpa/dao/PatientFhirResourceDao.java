cffc95bf773a28b599e4e4543e619cb4d2e71c01
Ismael Sarmento <ismael.sarmento.jr@gmail.com>
2015-06-15 13:18:21 -0400

    Implemented Search with no params for Location

--- gt-fhir-webapp/src/main/java/edu/gatech/i3l/fhir/jpa/dao/PatientFhirResourceDao.java
+++ gt-fhir-webapp/src/main/java/edu/gatech/i3l/fhir/jpa/dao/PatientFhirResourceDao.java
@@ -55,25 +55,6 @@ public class PatientFhirResourceDao extends BaseFhirResourceDao<Patient>{
 		
 	}
 
-//	@Override
-//	public DaoMethodOutcome create(T theResource) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public DaoMethodOutcome create(T theResource, String theIfNoneExist) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public DaoMethodOutcome create(T theResource, String theIfNoneExist,
-//			boolean thePerformIndexing) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-
 	@Override
 	public DaoMethodOutcome delete(IdDt theResource) {
 		// TODO Auto-generated method stub
@@ -131,65 +112,6 @@ public class PatientFhirResourceDao extends BaseFhirResourceDao<Patient>{
 	}
 
 	
-	
-//	protected void loadReverseIncludes(List<Long> theMatches, Set<Include> theRevIncludes) {
-//		if (theMatches.size() == 0) {
-//			return;
-//		}
-//
-//		HashSet<Long> pidsToInclude = new HashSet<Long>();
-//
-//		for (Include nextInclude : theRevIncludes) {
-//			boolean matchAll = "*".equals(nextInclude.getValue());
-//			if (matchAll) {
-//				String sql = "SELECT r FROM ResourceLink r WHERE r.myTargetResourcePid IN (:target_pids)";
-//				TypedQuery<ResourceLink> q = myEntityManager.createQuery(sql, ResourceLink.class);
-//				q.setParameter("target_pids", theMatches);
-//				List<ResourceLink> results = q.getResultList();
-//				for (ResourceLink resourceLink : results) {
-//					pidsToInclude.add(resourceLink.getSourceResourcePid());
-//				}
-//			} else {
-//				int colonIdx = nextInclude.getValue().indexOf(':');
-//				if (colonIdx < 2) {
-//					continue;
-//				}
-//				String resType = nextInclude.getValue().substring(0, colonIdx);
-//				RuntimeResourceDefinition def = getContext().getResourceDefinition(resType);
-//				if (def == null) {
-//					ourLog.warn("Unknown resource type in _revinclude=" + nextInclude.getValue());
-//					continue;
-//				}
-//
-//				String paramName = nextInclude.getValue().substring(colonIdx + 1);
-//				RuntimeSearchParam param = def.getSearchParam(paramName);
-//				if (param == null) {
-//					ourLog.warn("Unknown param name in _revinclude=" + nextInclude.getValue());
-//					continue;
-//				}
-//
-//				for (String nextPath : param.getPathsSplit()) {
-//					String sql = "SELECT r FROM ResourceLink r WHERE r.mySourcePath = :src_path AND r.myTargetResourcePid IN (:target_pids)";
-//					TypedQuery<ResourceLink> q = myEntityManager.createQuery(sql, ResourceLink.class);
-//					q.setParameter("src_path", nextPath);
-//					q.setParameter("target_pids", theMatches);
-//					List<ResourceLink> results = q.getResultList();
-//					for (ResourceLink resourceLink : results) {
-//						pidsToInclude.add(resourceLink.getSourceResourcePid());
-//					}
-//				}
-//			}
-//		}
-//
-//		theMatches.addAll(pidsToInclude);
-//	}
-	
-	
-	
-//	protected <T extends IResource> T toResource(IResourceTable theResourceTable) {
-//		return theResourceTable.getRelatedResource();
-//	}
-
 	
 
 	@Override
@@ -278,13 +200,24 @@ public class PatientFhirResourceDao extends BaseFhirResourceDao<Patient>{
 	public Predicate translatePredicateDateLessThan(String theParamName, Date upperBound, Root<? extends IResourceTable> from, CriteriaBuilder theBuilder) {
 		Calendar c = Calendar.getInstance();
 		c.setTime(upperBound);
-		Predicate lt1 = theBuilder.lessThan(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR));
-		Predicate lt2 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
-										theBuilder.lessThan(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)));
-		Predicate lt3 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
-										theBuilder.equal(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)),
-										theBuilder.lessThanOrEqualTo(from.get("dayOfBirth").as(Integer.class), c.get(Calendar.DAY_OF_MONTH)));
-		Predicate ub = theBuilder.or(lt1, lt2, lt3);
+		Predicate ub = null;
+		switch (theParamName) {
+		case Patient.SP_DEATHDATE:
+			ub = theBuilder.lessThanOrEqualTo(from.get("death_date").as(Date.class), upperBound);
+			break;
+		case Patient.SP_BIRTHDATE:
+			Predicate lt1 = theBuilder.lessThan(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR));
+			Predicate lt2 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
+					theBuilder.lessThan(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)));
+			Predicate lt3 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
+					theBuilder.equal(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)),
+					theBuilder.lessThanOrEqualTo(from.get("dayOfBirth").as(Integer.class), c.get(Calendar.DAY_OF_MONTH)));
+			ub = theBuilder.or(lt1, lt2, lt3);
+			
+			break;	
+		default:
+			break;
+		}
 		return ub;
 	}
 
@@ -293,13 +226,23 @@ public class PatientFhirResourceDao extends BaseFhirResourceDao<Patient>{
 			Date lowerBound, Root<? extends IResourceTable> from, CriteriaBuilder theBuilder) {
 		Calendar c = Calendar.getInstance();
 		c.setTime(lowerBound);
-		Predicate gt1 = theBuilder.greaterThan(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR));
-		Predicate gt2 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
-										theBuilder.greaterThan(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)));
-		Predicate gt3 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
-										theBuilder.equal(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)),
-										theBuilder.greaterThanOrEqualTo(from.get("dayOfBirth").as(Integer.class), c.get(Calendar.DAY_OF_MONTH)));
-		Predicate lb = theBuilder.or(gt1, gt2, gt3);
+		Predicate lb = null;
+		switch (theParamName) {
+		case Patient.SP_DEATHDATE:
+			lb = theBuilder.greaterThanOrEqualTo(from.get("death_date").as(Date.class), lowerBound);
+			break;
+		case Patient.SP_BIRTHDATE:
+			Predicate gt1 = theBuilder.greaterThan(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR));
+			Predicate gt2 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
+											theBuilder.greaterThan(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)));
+			Predicate gt3 = theBuilder.and( theBuilder.equal(from.get("yearOfBirth").as(Integer.class), c.get(Calendar.YEAR)),
+											theBuilder.equal(from.get("monthOfBirth").as(Integer.class), c.get(Calendar.MONTH)),
+											theBuilder.greaterThanOrEqualTo(from.get("dayOfBirth").as(Integer.class), c.get(Calendar.DAY_OF_MONTH)));
+			lb = theBuilder.or(gt1, gt2, gt3);
+			break;	
+		default:
+			break;
+		}
 		return lb;
 	}
 }
