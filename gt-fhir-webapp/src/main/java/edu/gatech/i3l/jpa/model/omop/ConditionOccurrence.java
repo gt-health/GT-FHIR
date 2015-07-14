6a93da24052ceb682ce64c9f6ad99eb461acae4d
myungchoi <myungchoi@gmail.com>
2015-07-14 16:39:55 +0900

    Merge branch 'condition_operations'

--- gt-fhir-webapp/src/main/java/edu/gatech/i3l/jpa/model/omop/ConditionOccurrence.java
+++ gt-fhir-webapp/src/main/java/edu/gatech/i3l/jpa/model/omop/ConditionOccurrence.java
@@ -37,16 +37,16 @@ public class ConditionOccurrence extends BaseResourceEntity {
 	private Provider provider;
 	private VisitOccurrence encounter;
 	private String sourceValue;
-	
+
 	public ConditionOccurrence() {
 		super();
 	}
-	
-	public ConditionOccurrence(Long id, Person person, Concept conditionConcept,
-			Date startDate, Date endDate, Concept conditionTypeConcept, String stopReason,
-			Provider provider, VisitOccurrence encounter, String sourceValue) {
+
+	public ConditionOccurrence(Long id, Person person, Concept conditionConcept, Date startDate, Date endDate,
+			Concept conditionTypeConcept, String stopReason, Provider provider, VisitOccurrence encounter,
+			String sourceValue) {
 		super();
-		
+
 		this.id = id;
 		this.person = person;
 		this.conditionConcept = conditionConcept;
@@ -58,17 +58,19 @@ public class ConditionOccurrence extends BaseResourceEntity {
 		this.encounter = encounter;
 		this.sourceValue = sourceValue;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see edu.gatech.i3l.jpa.model.omop.IResourceTable#getRelatedResource()
 	 */
 	@Override
 	public Condition getRelatedResource() {
 		Condition condition = new Condition();
 
-		// Populate condition parameters. 
-		// Refer to 4.3.3 at http://hl7-fhir.github.io/condition.html 
-		
+		// Populate condition parameters.
+		// Refer to 4.3.3 at http://hl7-fhir.github.io/condition.html
+
 		// set Identifier
 		condition.setId(getIdDt());
 
@@ -76,62 +78,69 @@ public class ConditionOccurrence extends BaseResourceEntity {
 		ResourceReferenceDt patientReference = new ResourceReferenceDt(person.getIdDt());
 		condition.setPatient(patientReference);
 
-		// Set encounter if exists. 
+		// Set encounter if exists.
 		if (encounter != null && encounter.getId() > 0) {
 			// FIXME: encounter resource not yet implemented.
-			//        we just create this reference resource manually. When encounter is implemented, we
-			//        will get it from visit_occurrence class.
+			// we just create this reference resource manually. When encounter
+			// is implemented, we
+			// will get it from visit_occurrence class.
 			ResourceReferenceDt encounterReference = new ResourceReferenceDt(encounter.getIdDt());
 			condition.setEncounter(encounterReference);
 		}
-		
+
 		// Set asserter if exists
-		// This can be either Patient or Practitioner. 
+		// This can be either Patient or Practitioner.
 		if (provider != null && provider.getId() > 0) {
 			ResourceReferenceDt practitionerReference = new ResourceReferenceDt(provider.getIdDt());
 			condition.setAsserter(practitionerReference);
 		}
 
 		// Set Code
-//		System.out.println("ConceptID:"+this.getConditionConcept().getId().toString());
-//		System.out.println("ConceptName:"+this.getConditionConcept().getName());
-//
-//		Vocabulary myVoc = this.getConditionConcept().getVocabulary();
-//		
-//		System.out.println("VocabularyID:"+myVoc.getId());
-//		System.out.println("VocabularyName:"+myVoc.getName());
+		// System.out.println("ConceptID:"+this.getConditionConcept().getId().toString());
+		// System.out.println("ConceptName:"+this.getConditionConcept().getName());
+		//
+		// Vocabulary myVoc = this.getConditionConcept().getVocabulary();
+		//
+		// System.out.println("VocabularyID:"+myVoc.getId());
+		// System.out.println("VocabularyName:"+myVoc.getName());
 
 		String theSystem = conditionConcept.getVocabulary().getSystemUri();
 		String theCode = conditionConcept.getConceptCode();
-		
+
 		CodeableConceptDt conditionCodeConcept = new CodeableConceptDt();
 		if (theSystem != "") {
-			// Create coding here. We have one coding in this condition as OMOP allows one coding concept per condition.
-			// In the future, if we want to allow multiple coding concepts here, we need to do it here.
+			// Create coding here. We have one coding in this condition as OMOP
+			// allows one coding concept per condition.
+			// In the future, if we want to allow multiple coding concepts here,
+			// we need to do it here.
 			CodingDt coding = new CodingDt(theSystem, theCode);
 			coding.setDisplay(conditionConcept.getName());
 			conditionCodeConcept.addCoding(coding);
 		}
 
-		// FHIR does not require the coding. If our System URI is not mappable from
-		// OMOP database, then coding would be empty. Set Text here. Even text is not
-		// required in FHIR. But, then no reason to have this condition, I think...
-		String theText = conditionConcept.getName() + ", "
-				+ conditionConcept.getVocabulary().getName() + ", "
+		// FHIR does not require the coding. If our System URI is not mappable
+		// from
+		// OMOP database, then coding would be empty. Set Text here. Even text
+		// is not
+		// required in FHIR. But, then no reason to have this condition, I
+		// think...
+		String theText = conditionConcept.getName() + ", " + conditionConcept.getVocabulary().getName() + ", "
 				+ conditionConcept.getConceptCode();
-		
+
 		conditionCodeConcept.setText(theText);
 		condition.setCode(conditionCodeConcept);
-		
+
 		// Set clinicalStatus
-		// We have clinicalStatus information in the FHIR extended table. This will
-		// be set in the extended class. 
-		
+		// We have clinicalStatus information in the FHIR extended table. This
+		// will
+		// be set in the extended class.
+
 		// Set severity
 		// We have this as well in the FHIR exteded table.
 
 		// Set onset[x]
-		// We may have only one date if this condition did not end. If ended, we have
+		// We may have only one date if this condition did not end. If ended, we
+		// have
 		// a period. First, check if endDate is available.
 		DateTimeDt startDateDt = new DateTimeDt(startDate);
 		if (endDate == null) {
@@ -145,7 +154,7 @@ public class ConditionOccurrence extends BaseResourceEntity {
 			periodDt.setEnd(endDateDt);
 			condition.setOnset(periodDt);
 		}
-		
+
 		return condition;
 	}
 
@@ -153,7 +162,9 @@ public class ConditionOccurrence extends BaseResourceEntity {
 		return Condition.class;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see ca.uhn.fhir.jpa.entity.BaseHasResource#getId()
 	 */
 	@Override
@@ -164,12 +175,14 @@ public class ConditionOccurrence extends BaseResourceEntity {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public IdDt getIdDt() {
 		return new IdDt(getResourceType(), id);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see ca.uhn.fhir.jpa.entity.BaseHasResource#getResourceType()
 	 */
 	@Override
@@ -180,71 +193,71 @@ public class ConditionOccurrence extends BaseResourceEntity {
 	public Person getPerson() {
 		return person;
 	}
-	
+
 	public void setPerson(Person person) {
 		this.person = person;
 	}
-	
+
 	public Concept getConditionConcept() {
 		return conditionConcept;
 	}
-	
+
 	public void setConditionConcept(Concept conditionConcept) {
 		this.conditionConcept = conditionConcept;
 	}
-	
+
 	public Date getStartDate() {
 		return startDate;
 	}
-	
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
-	
+
 	public Date getEndDate() {
 		return endDate;
 	}
-	
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
-	
+
 	public Concept getConditionTypeConcept() {
 		return conditionTypeConcept;
 	}
-	
+
 	public void setConditionTypeConcept(Concept conditionTypeConcept) {
 		this.conditionTypeConcept = conditionTypeConcept;
 	}
-	
+
 	public String getStopReason() {
 		return stopReason;
 	}
-	
+
 	public void setStopReason(String stopReason) {
 		this.stopReason = stopReason;
 	}
-	
+
 	public Provider getProvider() {
 		return provider;
 	}
-	
+
 	public void setProvider(Provider provider) {
 		this.provider = provider;
 	}
-	
+
 	public VisitOccurrence getEncounter() {
 		return encounter;
 	}
-	
+
 	public void setEncounter(VisitOccurrence encounter) {
 		this.encounter = encounter;
 	}
-	
+
 	public String getSourceValue() {
 		return sourceValue;
 	}
-	
+
 	public void setSourceValue(String sourceValue) {
 		this.sourceValue = sourceValue;
 	}
@@ -256,17 +269,17 @@ public class ConditionOccurrence extends BaseResourceEntity {
 
 			this.person = new Person();
 			this.person.setId(condition.getPatient().getReference().getIdPartAsLong());
-			
+
 			CodeableConceptDt codeableConcept = condition.getCode();
-			CodingDt code = codeableConcept.getCoding().get(0);				
+			CodingDt code = codeableConcept.getCoding().get(0);
 			this.conditionConcept = new Concept();
 			this.conditionConcept.setConceptCode(code.getCode());
 			String uri = code.getSystem();
-			
+
 			Vocabulary vocabularyCode = new Vocabulary();
 			vocabularyCode.setIdNameBySystemUri(uri);
 			this.conditionConcept.setVocabulary(vocabularyCode);
-			
+
 			IDatatype onSetDate = condition.getOnset();
 			if (onSetDate instanceof DateTimeDt) {
 				DateTimeDt dateTimeDt = (DateTimeDt) onSetDate;
@@ -280,11 +293,12 @@ public class ConditionOccurrence extends BaseResourceEntity {
 			this.conditionTypeConcept = new Concept();
 			ResourceReferenceDt encounterReference = condition.getEncounter();
 			Vocabulary vocabularyConditionType = new Vocabulary();
-			vocabularyConditionType.setId((long)37);
+			vocabularyConditionType.setId((long) 37);
 			this.conditionTypeConcept.setVocabulary(vocabularyConditionType);
 			if (encounterReference == null) {
-				// No Resource Reference for Encounter. 
-				// We just assumed that this is EHR Problem List Entry = 38000245
+				// No Resource Reference for Encounter.
+				// We just assumed that this is EHR Problem List Entry =
+				// 38000245
 				this.conditionTypeConcept.setConceptCode("38000245");
 			} else {
 				this.conditionTypeConcept.setConceptCode("44786627");
@@ -292,9 +306,10 @@ public class ConditionOccurrence extends BaseResourceEntity {
 				this.encounter = new VisitOccurrence();
 				this.encounter.setId(encounterId);
 			}
-			
-			// this.stopReason = stopReason;  NOTE: no FHIR parameter for stopReason.
-			
+
+			// this.stopReason = stopReason; NOTE: no FHIR parameter for
+			// stopReason.
+
 			ResourceReferenceDt asserterReference = condition.getAsserter();
 			if (asserterReference != null) {
 				if (asserterReference.getReference().getResourceType().equalsIgnoreCase(Provider.RESOURCE_TYPE)) {
@@ -306,7 +321,7 @@ public class ConditionOccurrence extends BaseResourceEntity {
 
 			this.sourceValue = "FHIRCREATE";
 		}
-		
+
 		return this;
 	}
 
@@ -323,36 +338,20 @@ public class ConditionOccurrence extends BaseResourceEntity {
 	}
 
 	@Override
-	public String translateSearchParam(String chain) {
-		String translatedChain = "";
-		if(chain.isEmpty())
-			return translatedChain;
-		String head = "";
-		if(chain.contains(".")){
-			head = chain.substring(0, chain.indexOf("."));
-			chain = chain.substring(chain.indexOf(".") + 1, chain.length());
-		} else {
-			head = chain;
-		}
-		
-		System.out.println("translateLink():chain="+chain);
-		switch (head) {
+	public String translateSearchParam(String link) {
+		switch (link) {
 		case Condition.SP_ASSERTER:
-			translatedChain = translatedChain.concat(this.provider.translateSearchParam(head));
-			System.out.println("translateLink():Asserter:translateChain="+translatedChain);
-			break;
+			System.out.println("translateLink():Asserter=" + link);
+			return "provider";
 		case Condition.SP_ENCOUNTER:
-			translatedChain = translatedChain.concat(this.encounter.translateSearchParam(head));
-			System.out.println("translateLink():Encounter:translateChain="+translatedChain);
-			break;
+			System.out.println("translateLink():Encounter=" + link);
+			return "encounter";
 		case Condition.SP_PATIENT:
-			translatedChain = translatedChain.concat(this.person.translateSearchParam(head));
-			System.out.println("translateLink():Patient:translateChain="+translatedChain);
-			break;
+			System.out.println("translateLink():Patient=" + link);
+			return "person";
 		default:
 			break;
 		}
-		
-		return translatedChain;
+		return link;
 	}
 }
