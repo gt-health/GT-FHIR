fd10bbefc6022fe15d10bfe067750b7f83831fdb
Myung Choi <myung.choi@gtri.gatech.edu>
2015-08-11 23:48:58 -0400

    Pushing to save.

--- gt-fhir-webapp/src/main/java/edu/gatech/i3l/hl7/fhir/security/OIDCInterceptor.java
+++ gt-fhir-webapp/src/main/java/edu/gatech/i3l/hl7/fhir/security/OIDCInterceptor.java
@@ -20,6 +20,7 @@ import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.types.ParameterStyle;
 import org.apache.oltu.oauth2.rs.request.OAuthAccessResourceRequest;
+import org.springframework.web.client.RestTemplate;
 
 import ca.uhn.fhir.rest.method.OtherOperationTypeEnum;
 import ca.uhn.fhir.rest.method.RequestDetails;
@@ -36,15 +37,19 @@ public class OIDCInterceptor extends InterceptorAdapter {
 			.getLogger(OIDCInterceptor.class);
 
 	private int myTimeSkewAllowance = 300;
-	private DataSource dataSource;
+	private String introspectURL;
+	private String clientId;
+	private String clientSecret;
+	
+//	private DataSource dataSource;
 
 	public OIDCInterceptor() {
-		try {
-			dataSource = (DataSource) new InitialContext()
-					.lookup("java:/comp/env/jdbc/oidc");
-		} catch (NamingException e) {
-			e.printStackTrace();
-		}
+//		try {
+//			dataSource = (DataSource) new InitialContext()
+//					.lookup("java:/comp/env/jdbc/oidc");
+//		} catch (NamingException e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	@Override
@@ -61,7 +66,7 @@ public class OIDCInterceptor extends InterceptorAdapter {
 			return true;
 		}
 		
-		// Quick Hack for request from localhost
+		// Quick Hack for request from localhost overlay site.
 		if (theRequest.getRemoteAddr().equalsIgnoreCase("127.0.0.1") ||
 				theRequest.getRemoteAddr().equalsIgnoreCase("0:0:0:0:0:0:0:1")) {
 			return true;
@@ -74,42 +79,45 @@ public class OIDCInterceptor extends InterceptorAdapter {
 
 			// Get the access token
 			String accessToken = oauthRequest.getAccessToken();
-
 			System.out.println(accessToken);
 			
+			// Introspect the token
+			RestTemplate restTemplate = new RestTemplate();
+			
 			// Validate the accessToken.
-			String SQL_STATEMENT = "SELECT c.client_id AS client_id, t.token_value AS token_value, t.expiration AS token_expiration, c.client_name AS client_name FROM client_details c, access_token t WHERE t.token_value='"
-					+ accessToken + "' and t.client_id = c.id;";
+//			String SQL_STATEMENT = "SELECT c.client_id AS client_id, t.token_value AS token_value, t.expiration AS token_expiration, c.client_name AS client_name FROM client_details c, access_token t WHERE t.token_value='"
+//					+ accessToken + "' and t.client_id = c.id;";
+//
+//			System.out.println(SQL_STATEMENT);
+//			connection = dataSource.getConnection();
+//
+//			statement = connection.createStatement();
+//			ResultSet rs = statement.executeQuery(SQL_STATEMENT);
+//
+//			if (rs.next()) {
+//				System.out.println("checking expiration time");
+//				Timestamp ts = rs.getTimestamp("token_expiration");
+//				if (ts != null) {
+//					Date minAllowableExpirationTime = new Date(
+//							System.currentTimeMillis()
+//									- (myTimeSkewAllowance * 1000L));
+//					if (ts.before(minAllowableExpirationTime)) {
+//						// expired.
+//						err_msg = "Current Time:"+minAllowableExpirationTime.toString()+" Access Token is expired: " + ts.toString();
+//						System.out.println(err_msg);
+//					} else {
+//						System.out.println("Auth successful");
+//					}
+//				}
+//			} else {
+//				System.out.println("Access Token does not exist");
+//				err_msg = "Access Token Does Not Exist";
+//			}
 
-			System.out.println(SQL_STATEMENT);
-			connection = dataSource.getConnection();
-
-			statement = connection.createStatement();
-			ResultSet rs = statement.executeQuery(SQL_STATEMENT);
-
-			if (rs.next()) {
-				System.out.println("checking expiration time");
-				Timestamp ts = rs.getTimestamp("token_expiration");
-				if (ts != null) {
-					Date minAllowableExpirationTime = new Date(
-							System.currentTimeMillis()
-									- (myTimeSkewAllowance * 1000L));
-					if (ts.before(minAllowableExpirationTime)) {
-						// expired.
-						err_msg = "Current Time:"+minAllowableExpirationTime.toString()+" Access Token is expired: " + ts.toString();
-						System.out.println(err_msg);
-					} else {
-						System.out.println("Auth successful");
-					}
-				}
-			} else {
-				System.out.println("Access Token does not exist");
-				err_msg = "Access Token Does Not Exist";
-			}
-
-		} catch (OAuthSystemException | OAuthProblemException | SQLException e) {
+		} catch (OAuthSystemException | OAuthProblemException e) {
 			e.printStackTrace();
-			if (err_msg.isEmpty()) err_msg = "Access Token Validation Failed";
+			if (err_msg.isEmpty()) err_msg = e.toString();
+			else err_msg.concat(e.toString());
 		} finally {
 			try {
 				connection.close();
@@ -128,6 +136,30 @@ public class OIDCInterceptor extends InterceptorAdapter {
 			System.out.println("Error:"+err_msg);
 			throw new AuthenticationException(err_msg);
 		}
+	}
+	
+	public String getIntrospectURL() {
+		return introspectURL;
+	}
+	
+	public void setIntrospectURL(String introspectURL) {
+		this.introspectURL = introspectURL;
+	}
+	
+	public String getClientId() {
+		return clientId;
+	}
+	
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	
+	public String getClientSecret() {
+		return clientSecret;
+	}
+	
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
 	}
 
 }
